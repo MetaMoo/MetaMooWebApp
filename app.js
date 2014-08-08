563ab5cbcb95276534209db7a107b5501e0cba1b
vasanthsarathy <vsarathy@gmail.com>
2014-08-08 11:38:39 -0400

    Bootstrap layout for search bar and results

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var path = require('path');
-
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+// middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
