bf97097c49bd3c88baa9b6330fa7be8ee9c103d7
vasanthsarathy <vsarathy@gmail.com>
2014-08-09 18:01:00 -0400

    Can now pass a variable from search box through to routes and back out to browser inside table

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,19 @@
+
+// JQuery function that gets info from route /searching passing along the
+// string that user typed from the search box. The output is a table of data
+// including the value of the search string itself.  Here "data" is the returned
+// value from the /searching route. 
 $(function(){
   $('#search').on('keyup',function(e){
     if(e.keyCode === 13) {
     var parameters = { search: $(this).val() };
       $.get( '/searching',parameters, function(data) {
-        $('#results').html(data);
+        //$('#results').html(data);
+        $('#resultsTable tr').remove();
+        $('#resultsTable').append('<tr><th> Content </th> <th>Tag</th> <th>Note</th> <th>Source</th><th>Date</th></tr>');
+        for (var h=0; h<data.length; h++){
+          $('#resultsTable').append('<tr><td>'+data[h].content +'</td> <td>'+data[h].tag +'</td> <td>'+data[h].note +'</td> <td>'+data[h].source +'</td> <td>'+data[h].date +'</td> </tr>');
+        }
       });
     };
   });
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,36 @@ router.get('/searching', function(req, res) {
 	//var snippets = [{content: "Test snippet of data", tag: "#awesome"},{content: "Test 2 snippet of data", tag: "#cool"}];
 
  var val = req.query.search;
- res.send(val);
+
+// need to include some function here that takes "val" as input and outputs 
+// all the objects from metamoo_data.json into a variable called "data"
+
+var data = [
+  {
+    "content": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
+    "tag": "one",
+    "note": "Maecenas pulvinar lobortis est. Phasellus sit amet erat.",
+    "date": "7/20/2014",
+    "source": "sakura.ne.jp"
+  },
+  {
+    "content": "Nulla justo.",
+    "tag": "two",
+    "note": "In blandit ultrices enim.",
+    "date": "4/6/2014",
+    "source": "mtv.com"
+  },
+  {
+    "content": "Pellentesque ultrices mattis odio. Donec vitae nisi.",
+    "tag": val,
+    "note": "Nam dui.",
+    "date": "6/1/2014",
+    "source": "who.int"
+  }];
+
+ //search val against appdata
+ res.send(data);
+
  //console.log(val);
 
 });
