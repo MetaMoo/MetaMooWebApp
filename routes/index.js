f2ec750d1a50ecae68eccae8502209fd18d31da9
vasanthsarathy <vsarathy@gmail.com>
2014-08-18 16:24:38 -0400

    Added a route to allow clients to check if user is authenticated

--- routes/index.js
+++ routes/index.js
@@ -225,6 +225,10 @@ router.post('/snippet', isLoggedIn, function(req, res){
     });
 
 
+router.get('/ping', isLoggedIn, function(req, res){
+	res.send( {loggedIn : true});
+});
+
 
 // Route middleware to ensure user is logged in
 function isLoggedIn(req, res, next){
