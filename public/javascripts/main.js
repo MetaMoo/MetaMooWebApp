45ae6251e6c61429d6244106a99106e68c8db6d1
vasanthsarathy <vsarathy@gmail.com>
2014-10-03 09:02:33 -0400

    Included edit options in Show All page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -38,9 +38,9 @@ $(function(){
       $.get( '/searching',parameters, function(data) {
         //$('#results').html(data);
         $('#resultsTable tr').remove();
-        $('#resultsTable').append('<tr><th style="width:30%"> Content </th> <th style="width:15%">Tag</th> <th>Note</th style="width:20%"> <th style="width:20%">Source</th><th style="width:15%">Date</th></tr>');
+       $('#resultsTable').append('<tr><th> Highlighted Content </th> <th>Tag</th> <th>Note</th> <th>Source</th><th>Date</th></tr>');
         for (var h=0; h<data.length; h++){
-          $('#resultsTable').append('<tr><td >'+data[h].content +'</td> <td>'+data[h].tag +'</td> <td>'+data[h].note +'</td> <td>'+ '<a target="_blank" href="'+data[h].source+'">'+data[h].source+'</a>' +'</td> <td>'+data[h].date +'</td> </tr>');
+          $('#resultsTable').append('<tr class="edit_tr" id="'+data[h]._id+'"><td >'+data[h].content +'</td> <td class="edit_td"><span class="texty" id="tag_'+data[h]._id+'">'+data[h].tag +'</span><input type="text" class="editbox" id="tag_input_'+data[h]._id+'" value="'+data[h].tag+'"</td> <td class="edit_td"><span class="texty" id="note_'+data[h]._id+'">'+data[h].note +'</span><input type="text" class="editbox" id="note_input_'+data[h]._id+'" value="'+data[h].note+'"</td><td>'+ '<a target="_blank" href="'+data[h].source+'">'+data[h].source+'</a>' +'</td> <td>'+data[h].date +'</td> </tr>');
         }
       }); 
     }
